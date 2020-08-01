56c5c689f0a976ae70ccc4644e7469ce9519765a
sastelvio <41056057+sastelvio@users.noreply.github.com>
2020-08-01 17:21:33 +0200

    Add files via upload

---
+++ js/avatar-manager.js
@@ -0,0 +1,26 @@
+( function( $ ) {
+    $( document ).ready( function() {
+        $( '#your-profile' ).attr( 'enctype', 'multipart/form-data' );
+
+        // Disables upload buttons until files are selected
+        ( function() {
+            var button, input, fieldset = $( 'fieldset' );
+
+            if ( ! fieldset.length )
+                return;
+
+            button = fieldset.find( 'input[type="submit"]' );
+            input  = fieldset.find( 'input[type="file"]' );
+
+            function toggleUploadButton() {
+                button.prop( 'disabled', '' === input.map( function() {
+                    return $( this ).val();
+                } ).get().join( '' ) );
+            }
+
+            toggleUploadButton();
+
+            input.on( 'change', toggleUploadButton );
+        } )();
+    } );
+} )( jQuery );
